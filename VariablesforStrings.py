0751d407106cd0ab3a38fae4c16e5f2ab1d2b752
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-10 22:42:58 -0400

    inital commit halfway completd assigment

---
+++ VariablesforStrings.py
@@ -0,0 +1,20 @@
+# Because of the quotation marks, Python identifies the greeting below as a _______ (1 word, lowercase)
+# "Hello, World!"
+import string
+from tkinter import Variable
+string
+# Because there are no quotation marks, Python identifies the set of characters below as a _______.
+# country_of_origin
+Variable
+# Assign the string "Boo!" to the variable scare.
+scare = "Boo!"
+# Assign the string "jay" to the variable bird.
+bird = "jay"
+# Assign your first name to a variable. You choose the variable name.
+name = "justin"
+# Assign a string to a variable. You choose the variable name and the string.
+variable_name = "Emanuel"
+# I assign a string to a variable. Using the variable, write the statement that displays the string on the screen.
+my_name = "Mark"
+print(my_name)
+# Assign a string to a variable. You choose the variable name and the string. Using the variable, write a statement that displays the string on the screen.
